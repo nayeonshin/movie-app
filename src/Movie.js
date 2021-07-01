8a0fb0200d38be8a1b44e4043f0c82e97a8d9151
Nayeon Shin <dev.nayeon.shin@gmail.com>
2021-07-01 09:59:21 +0900

    feat: Add array slicing and bring back Movie.css

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({ title, year, rating, runtime, genres, summary, poster }) {
   return (
@@ -15,7 +16,7 @@ function Movie({ title, year, rating, runtime, genres, summary, poster }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
