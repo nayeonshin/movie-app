5b5e31268894a504fc7e45e5c6aa4437ee70d0a9
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-20 03:39:02 +0900

    style: Alphabetize imports

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./routes/Home";
+
 import About from "./routes/About";
+import { BrowserRouter, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
+
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Route path="/movie-app" exact={true} component={Home} />
+      <Route path="/movie-app" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/movie/:id" component={Detail} />
     </BrowserRouter>
