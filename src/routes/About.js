ff9625221303a228cab471d63e2aca55e33c11c7
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-21 02:42:53 +0900

    feat(About.js): Change contents

--- src/routes/About.js
+++ src/routes/About.js
@@ -5,11 +5,8 @@ import "./About.css";
 function About() {
   return (
     <div className="about__container">
-      <p>
-        “Freedom is the freedom to say that two plus two make four. If that is
-        granted, all else follows.”
-      </p>
-      <span>− George Orwell, 1984</span>
+      <p>Movie App is a simple React app that lists movies from YTS.</p>
+      <span>− Nayeon</span>
     </div>
   );
 }
