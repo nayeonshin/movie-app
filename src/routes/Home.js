53fad3e08a18411753b014cfa5ade12da2381746
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-20 22:24:55 +0900

    style(Home): Add line breaks between imports

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 import axios from "axios";
+
 import Movie from "../components/Movie";
+
 import "./Home.css";
+
 class Home extends React.Component {
   state = {
     isLoading: true,
