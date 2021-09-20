3224485d9eb61f44d93e24e16349a6a7dbf5e082
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-21 00:29:48 +0900

    feat(css): Create Detail.css

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 
+import "./Detail.css";
+
 class Detail extends React.Component {
   componentDidMount() {
     const { location, history } = this.props;
@@ -12,7 +14,19 @@ class Detail extends React.Component {
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <h1>{location.state.title}</h1>;
+      return (
+        <>
+          <h1 id="movie-title">{location.state.title}</h1>
+          <div className="movie-details">
+            <img alt="Poster" src={location.state.poster} />
+            <div className="movie-details__text">
+              <span>{location.state.year}</span>
+              <span>{location.state.genres}</span>
+              <p className="text__summary">{location.state.summary}</p>
+            </div>
+          </div>
+        </>
+      );
     } else {
       return null;
     }
