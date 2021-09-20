f7ff676b02749bce6e078b75f9cdbcff7bfdb0de
Nayeon <dev.nayeon.shin@gmail.com>
2021-09-21 04:27:58 +0900

    fix(routing): Replace BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import About from "./routes/About";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
@@ -10,12 +10,12 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navigation />
-      <Route path={"/movie-app/"} exact={true} component={Home} />
-      <Route path={"/movie-app/about"} component={About} />
-      <Route path={"/movie-app/movie/:id"} component={Detail} />
-    </BrowserRouter>
+      <Route path={"/"} exact={true} component={Home} />
+      <Route path={"/about"} component={About} />
+      <Route path={"/movie/:id"} component={Detail} />
+    </HashRouter>
   );
 }
 
